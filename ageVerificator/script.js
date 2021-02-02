330138ccdce47127eeeff3c664325b80871fd6d5
Spokay <hbressange@edenschool.fr>
2021-02-02 17:15:55 +0100

    third commit

--- ageVerificator/script.js
+++ ageVerificator/script.js
@@ -1,12 +1,29 @@
 
+    // let age = prompt("How old are you ?");
+    //
+    // if(age == null || age === ""){
+    //     alert("you didn\'t answer to the question");
+    // }
+    // else if(isNaN(age)){
+    //     alert("You didn\'t answered a number");
+    // }
+    // else{
+    //     alert("You are " + age + " years old");
+    // }
     let age = prompt("How old are you ?");
-
-    if(age == null || age === ""){
-        alert("you didn\'t answer to the question");
-    }
-    else if(isNaN(age)){
-        alert("You didn\'t answered a number");
+    age = parseInt(age);
+    if (isNaN(age)){
+            alert("you didn\'t answer to the question");
     }
     else{
-        alert("You are " + age + " years old");
+        if (age >= 16){
+            alert("welcome");
+        }
+        else{
+            location.replace("https://www.edenschool.fr/");
+        }
     }
+
+
+
+
