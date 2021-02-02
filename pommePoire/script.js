6028380c2301979e3e79f5e1e6f512536d584e99
Spokay <hbressange@edenschool.fr>
2021-02-02 15:05:57 +0100

    initial commit

---
+++ pommePoire/script.js
@@ -0,0 +1,6 @@
+let poire = "poires";
+let poidPoire = 0.8;
+let pomme = "pommes";
+let poidPomme = 0.7;
+
+alert("Vous avez acheté des " + pomme + " et des " + poire + " pour un poid total de " + (poidPoire + poidPomme) + "kg")
